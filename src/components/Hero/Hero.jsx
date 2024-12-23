d166604e12c84e4dcab4631ffb79ee69aa97c8cf
Udaybisone <udaybisone01@gmail.com>
2024-12-23 21:19:25 +0530

    added Icon

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { SiLeetcode } from "react-icons/si";
 
 import styles from "./Hero.module.css";
 import { getImageUrl } from "../../utils";
@@ -15,19 +16,24 @@ export const Hero = () => {
         <div className={styles.logos}>
 
           <a href="https://github.com/Udaybisone" target="_blank">
-            <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+            <img src={getImageUrl("contact/leetcode.png")} alt="Leetcode"/>
           </a>
 
-          <a href="https://www.linkedin.com/in/uday-bisone-95aa21259/" target="_blank">
-            <img src={getImageUrl("contact/linkedinIcon.png")} alt="LinkedIn icon" />
+          <a href="https://leetcode.com/u/udaybisone/" target="_blank">
+            <img src={getImageUrl("contact/codechef.png")} alt="Github icon" />
           </a>
 
           <a href="mailto:udaybisone10@gmail.com" target="_blank">
-            <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+            <img src={getImageUrl("contact/gfg.png")} alt="Email icon" />
+          </a>
+
+          <a href="mailto:udaybisone10@gmail.com" target="_blank">
+            <img src={getImageUrl("contact/codeforces.png")} alt="Email icon" />
           </a>
         </div>
+
         <a href="./Uday_CV.pdf" download="uday.resume" className={styles.contactBtn}>
-          Download CV
+          Explore My Potential
         </a>
       </div>
       <img
